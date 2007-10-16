e459bd58162c6edc63f7329d9a14e41159f4e26f
awgh <awgh@1fecd473-a135-0410-bd75-fbe9beee1f7f>
2007-10-16 20:51:57 +0000

    Makefile clean-up.  I hate autoconf.

--- src/scm/demo2.scm
+++ src/scm/demo2.scm
@@ -1,10 +1,10 @@
 
 (define (load x) #t); this file is compile-only
 
-(include "src/scm/table.scm")
-(include "src/scm/default-fcns.scm")
-(include "src/scm/ethernet.scm")
-(include "src/scm/loopback.scm")
-(include "src/scm/ip4.scm")
-(include "src/scm/tcp.scm")
-(include "src/scm/rand.scm")
+(include "table.scm")
+(include "default-fcns.scm")
+(include "ethernet.scm")
+(include "loopback.scm")
+(include "ip4.scm")
+(include "tcp.scm")
+(include "rand.scm")
