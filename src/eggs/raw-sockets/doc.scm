8bc79f64686f42d16026818ef2a6fe3bcdccd643
elflng <elflng@1fecd473-a135-0410-bd75-fbe9beee1f7f>
2007-11-07 04:14:06 +0000

    version 2.0 final!

--- src/eggs/raw-sockets/doc.scm
+++ src/eggs/raw-sockets/doc.scm
@@ -2,13 +2,13 @@
 ;;;; file:     doc.scm
 ;;;; author:   Lenny Frank <elf@ephemeral.net>
 ;;;; author:   Benjamin L. Kurtz <bk2@alum.wpi.edu>
-;;;; date:     18 Sep 2007
+;;;; date:     06 Nov 2007
 ;;;; licence:  BSD (see LICENCE)
-;;;; version:  3.0
+;;;; version:  2.0
 ;;;; purpose:  eggdoc-formatted documentation for the raw-sockets egg
 ;;;;
-;;;; history:  3.0  Updated for v3.0 raw-sockets (elf)
-;;;;           2.1  Updated for v2.1 rawsock (elf)
+;;;; history:  2.0  Updated for v2.0 raw-sockets (elf)
+;;;;           1.1  Updated for v1.1 rawsock (elf)
 ;;;;           1.0  Initial release (bk2)
 
 
@@ -24,15 +24,16 @@
         (description (p "UNIX raw socket interface"))
         (author      (p (url "mailto:elf@ephemeral.net" "Elf") ","
                         (url "mailto:bk2@alum.wpi.edu"  "Benjamin L. Kurtz")))
-        (requires    (url "srfi-66.html" "srfi-66"))
 
         (history
             (version "1.0" "Initial release (Ben Kurtz)")
-            (version "2.0" "Refactored and cleaned (Elf)")
-            (version "2.1" "Added non-blocking reads (Elf)")
-            (version "3.0" "Changed read/write semantics, "
+            (version "1.1" "Refactored and cleaned (Elf)")
+            (version "1.2" "Added non-blocking reads (Elf)")
+            (version "1.3" "Changed read/write semantics, "
                            "added buffering of packets, "
-                           "switched to SIGIO instead of polling (Elf)"))
+                           "switched to SIGIO instead of polling (Elf)")
+            (version "2.0" "Switched to a threading model (SIGIO broken), "
+                           "lots of cleanup, profiling (Elf)"))
 
         (usage)
         (download "raw-sockets.egg")
@@ -44,6 +45,9 @@
                "allowing manual setting of Layer 2 fields.")
             (p "Currently, there is support for Linux and OS X.  If you want "
                "something else, send us the code for getting packet sockets.")
+            (p "raw-sockets currently uses a threaded model for reading and "
+               "writing.  See the documentation for the individual functions "
+               "for information on when and how they are activated.")
             
             (section "Opening and querying a packet socket"
                 (group
@@ -51,12 +55,7 @@
                         "(open-raw-socket INTERFACE)"
                         (p "Creates a new raw-socket object.  "
                            (tt "INTERFACE") " is the name of the network "
-                           "interface, given as a string.")
-                        (p "Note: Upon creation of the first raw socket, a "
-                           "SIGIO handler will be installed.  If a handler "
-                           "already exists, it will still be called after "
-                           "the new handler, if the new handler is not "
-                           "applicable."))
+                           "interface, given as a string."))
                     (procedure
                         "(raw-socket? OBJ)"
                         (p "Returns #t if OBJ is a raw-socket."))
@@ -67,20 +66,10 @@
 
             (section "Information on a packet socket"
                 (group
-                    (procedure "(raw-socket-domain OBJ)")
-                    (procedure "(raw-socket-type OBJ)")
-                    (procedure 
-                        "(raw-socket-protocol OBJ)"
-                        (p "Returns the socket domain/type/protocol associated "
-                           "with this raw-socket."))
                     (procedure
                         "(raw-socket-fd OBJ)"
                         (p "Returns the file descriptor associated with this "
                            "raw-socket."))
-                    (procedure
-                        "(raw-socket-saddr OBJ)"
-                        (p "Returns a pair (saddr . size) for the sockaddr "
-                           "structure and length of this raw-socket."))
                     (procedure
                         "(raw-socket-iface OBJ)"
                         (p "Returns the interface associated with this "
@@ -89,7 +78,11 @@
                         "(raw-socket-mtu OBJ)"
                         (p "Returns the MTU for this raw-socket.  On MacOS, "
                            "this is 1 less than the actual MTU size because of "
-                           "its broken network stack."))))
+                           "its broken network stack."))
+                    (procedure
+                        "(raw-socket-wqueue OBJ)"
+                        (p "Returns the writing queue associated with this "
+                           "raw-socket."))))
 
             (section "Writing to a packet socket"
                 (group
@@ -99,7 +92,11 @@
                            (tt "PACKET") " may be a u8vector or string.")
                         (p "Note: if the socket is not ready for writing, "
                            "the packet will be queued until writing is allowed "
-                           "or until the socket closes."))))
+                           "or until the socket closes."))
+                    (procedure
+                        "(raw-socket-send-flush RAW-SOCKET)"
+                        (p "Blocks the main thread until the writing queue is "
+                           "empty."))))
 
             (section "Reading from a packet socket"
                 (p "Reading from raw sockets is not done directly; instead, "
@@ -112,6 +109,8 @@
                    (tt "LENGTH") " is the length of DATA.")
                 (p "Multiple receiver procedures can be installed, and will "
                    "be called in most-recently-added-first order.")
+                (p "The reading thread is not activated until the first reader "
+                   "procedure is installed.")
                 (group
                     (procedure
                         "(raw-socket-add-recver RAW-SOCKET LABEL HANDLER)"
@@ -132,6 +131,23 @@
                         (p "Returns an ordered list of (LABEL . HANDLER) pairs "
                            "corresponding to all receiver procedures for "
                            "RAW-SOCKET."))))
+
+            (section "Thread procedures"
+                (group
+                    (procedure "(raw-socket-rmutex OBJ)")
+                    (procedure "(raw-socket-rthread OBJ)")
+                    (procedure "(raw-socket-wmutex OBJ)")
+                    (procedure
+                        "(raw-socket-wthread OBJ)"
+                        (p "Returns the read/write mutex or thread associated "
+                           "with the given raw socket.  It should rarely, if "
+                           "ever, be necessary to use these."))
+                    (macro
+                        "(raw-protect-region WRITER? RAW-SOCKET BODY ...)"
+                        (p "Handles creating and ending a mutex-protected "
+                           "region defined by BODY ... for the given "
+                           "RAW-SOCKET.  WRITER? should be #t if it is a "
+                           "writing call, #f otherwise."))))
 
             (section "Closing a packet socket"
                 (group
